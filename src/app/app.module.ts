21dbcf4215ce2d0a3d18e9780cc8abb7bd5bf7d5
Rituparna Saha <rituparnasaha54@gmail.com>
2023-04-02 14:11:26 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { Practice1Component } from './practice1/practice1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    Practice1Component
   ],
   imports: [
     BrowserModule,
